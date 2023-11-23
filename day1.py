289884f98284374fe6d155a8fe4157a1430843c7
Jakub <kuborgc6@gmail.com>
2023-11-23 15:22:09 +0100

    Finished part Two of Day One

--- day1.py
+++ day1.py
@@ -3,11 +3,15 @@ Lines = file1.readlines()
 
 values = [int(i) for i in Lines]
 values.sort()
-
+result = list()
 for i in range(len(values)):
     for j in range(i+1,len(values)):
-        if values[i] + values[j] == 2020:
-            result = values[i] * values[j]
         if values[i] + values[j] > 2020:
             break
+        for k in range(j+1,len(values)):
+            if values[i] + values[j] + values[k] == 2020:
+                result.append(values[i] * values[j] * values[k])
+            if values[i] + values[j] + values[k] > 2020:
+                break
 print (result)
+
