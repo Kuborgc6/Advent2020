15d010c3f231bf36b82b49d49e0d6eb4cf690124
Jakub <kuborgc6@gmail.com>
2023-11-26 21:11:23 +0100

    Finnished Day 6

--- day6.py
+++ day6.py
@@ -1,4 +1,22 @@
 #file = open('test_input.txt', 'r')
 file = open('input.txt', 'r')
-lines = [ line.split()[0] for line in file]
+lines = [ line.split() for line in file]
 #lines = file.readlines()
+lines.append([])
+
+#print(lines)
+result = 0
+temp = set()
+first_obj = True
+for line in lines:
+    if line and first_obj == True:
+        temp = set(line[0])
+        first_obj = False
+    elif line:
+        temp = temp.intersection(set(line[0]))
+    else:
+        result += len(temp)
+        temp = set()
+        first_obj = True
+
+print(result)
