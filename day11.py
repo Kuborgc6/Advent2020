7cf25f177ade48b39e3b4b94c36117bbc93ee26a
Jakub <kuborgc6@gmail.com>
2023-11-29 11:37:04 +0100

    Started to work on day 11

---
+++ day11.py
@@ -0,0 +1,8 @@
+file = open('input.txt', 'r')
+lines = [ int(line.split()[0]) for line in file]
+# lines.append([])
+
+# for i in range(0, len(lines)):
+#     lines[i] = int(lines[i][0])
+
+# print(lines)
