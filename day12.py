6e0e331b6a862e79ec1790026048572e1b250147
Jakub <kuborgc6@gmail.com>
2023-11-29 19:38:40 +0100

    Finnished Part Two of Day 12

--- day12.py
+++ day12.py
@@ -7,26 +7,38 @@ lines = [line.split()[0] for line in file]
 
 current_way = 0
 corr = [0, 0]
+waypoint = [10, 1]
 
-positions = ['E', 'S', 'W', 'N']
-positions_lib = {'E': 0.5, 'S': -1, 'W': -0.5, 'N': 1}
 change_position = ['R', 'L']
 change_position_library = {90: 1, 180: 2, 270: 3}
 change_position_library_RL = {'R': 1, 'L': -1}
 
 for line in lines:
     if line[0] in change_position:
-        current_way = (current_way + change_position_library[int(line[1:])]*change_position_library_RL[line[0]])%4
-    elif line[0] == 'E':
-        corr[0] += int(line[1:])
-    elif line[0] == 'W':
-        corr[0] -= int(line[1:])
-    elif line[0] == 'N':
-        corr[1] += int(line[1:])
-    elif line[0] == 'S':
-        corr[1] -= int(line[1:])
-    else:
-        way = positions_lib[positions[current_way]]
-        corr[abs(round(way))] += way/abs(way)*int(line[1:])
+        next_way = change_position_library[int(line[1:])]*change_position_library_RL[line[0]]
 
-print(abs(corr[0])+abs(corr[1]))
+        if next_way == 1 or next_way == -3:
+            temp = waypoint[0]
+            waypoint[0] = waypoint[1]
+            waypoint[1] = -temp
+        elif next_way == 2 or next_way == -2:
+            waypoint[0] = -waypoint[0]
+            waypoint[1] = -waypoint[1]
+        else:
+            temp = waypoint[0]
+            waypoint[0] = -waypoint[1]
+            waypoint[1] = temp
+
+    elif line[0] == 'E':
+        waypoint[0] += int(line[1:])
+    elif line[0] == 'W':
+        waypoint[0] -= int(line[1:])
+    elif line[0] == 'N':
+        waypoint[1] += int(line[1:])
+    elif line[0] == 'S':
+        waypoint[1] -= int(line[1:])
+    else:
+        corr[0] += waypoint[0]*int(line[1:])
+        corr[1] += waypoint[1]*int(line[1:])
+
+print(abs(corr[0]) + abs(corr[1]))
